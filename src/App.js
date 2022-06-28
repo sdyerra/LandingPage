f6bc303cd490269d74163e084e681657e70f81f1
sdyerra <saideepak.yerra@gmail.com>
2022-06-28 15:36:02 -0400

    added more design elements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
+import './Table'
 import house from './noun-house@2x.png';
 import create from './noun-plus@2x.png';
 import monthreview from './noun-agenda@2x.png';
 function App() {
   return (
     <div>
-      <p id="headerpic"></p>
-      <p class="armymed-1">ARMY <tspan class="armymed-2">MEDICINE</tspan></p>
+      <div class="armygrid">
+        <p id="headerpic"></p>
+        <p class="armymed-1">ARMY <tspan class="armymed-2">MEDICINE</tspan></p>
+        <hr class="armybar" size="2px" width="150px"></hr>
+        <p id="armyscarb">SCARB REVIEW</p>
+      </div>
       <div class="topnav">
         <a href="home"> <img alt="HOME" src={house} width="12" /> HOME</a>
         <a href="create"><img alt="CREATE" src={create} width="12" /> CREATE</a>
         <a href="monthreview"><img alt="REVIEW" src={monthreview} width="17" /> SCARB MONTHLY REVIEW SCHEDULE</a>
       </div>
-      <div></div>
-      <footer>
-        <p id="footer">2017, USA Medcom Acquisition Process</p>
-      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Table from './Table';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +10,9 @@ root.render(
   </React.StrictMode>
 );
 
+const tableirr = ReactDOM.createRoot(document.getElementById('tableirr'));
+tableirr.render(
+  <React.StrictMode>
+    <Table/>
+  </React.StrictMode>
+);
