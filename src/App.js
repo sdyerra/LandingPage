3bb5ce64c93b1b0d20d42045a18d13134099763f
sdyerra <saideepak.yerra@gmail.com>
2022-06-27 21:30:34 -0400

    initial web design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import house from './noun-house@2x.png';
+import create from './noun-plus@2x.png';
+import monthreview from './noun-agenda@2x.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p id="headerpic"></p>
+      <p class="armymed-1">ARMY <tspan class="armymed-2">MEDICINE</tspan></p>
+      <div class="topnav">
+        <a href="home"> <img alt="HOME" src={house} width="12" /> HOME</a>
+        <a href="create"><img alt="CREATE" src={create} width="12" /> CREATE</a>
+        <a href="monthreview"><img alt="REVIEW" src={monthreview} width="17" /> SCARB MONTHLY REVIEW SCHEDULE</a>
+      </div>
+      <div></div>
+      <footer>
+        <p id="footer">2017, USA Medcom Acquisition Process</p>
+      </footer>
     </div>
   );
 }
